9697bc1e49ccd2d84878a66ed38dcc16322c54e7
Paulb23 <p_batty@hotmail.co.uk>
2016-04-16 13:35:12 +0100

    basic casting

--- the_elemental_shapshifter/scritps/player.gd
+++ the_elemental_shapshifter/scritps/player.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+const FACE_UP = 0
+const FACE_DOWN = 1
+const FACE_LEFT = 2
+const FACE_RIGHT = 3
+
 const MOVE_UP = 0
 const MOVE_DOWN = 1
 const MOVE_LEFT = 2
@@ -11,6 +16,16 @@ const MODE_FIRE = 1
 const MODE_WATER = 2
 const MODE_EARTH = 3
 const MODE_AIR = 4
+const MODE_CAST = 5
+const MODE_CAST_NONE = 6
+
+const MODE_FIRE_CAST_TIME = 0
+const MODE_WATER_CAST_TIME = 0
+const MODE_EARTH_CAST_TIME = 1.0
+const MODE_AIR_CAST_TIME = 0
+
+var current_mode = 0
+var spell_to_cast = 6
 
 var speed = 10
 var time_to_move = 15;
@@ -21,16 +36,28 @@ var actual_pos = get_pos();
 
 func _ready():
 	set_fixed_process(true)
+	get_node("Timer").connect("timeout", self, "timer_end")
 	
 func _fixed_process(delta):
+	
+	if current_mode == MODE_NORMAL:
+		spell_to_cast = handle_cast_input()
+		
+		if (spell_to_cast != MODE_CAST_NONE):
+			current_mode = MODE_CAST
+			
+		if (spell_to_cast == MODE_EARTH):
+			get_node("Timer").set_wait_time(MODE_EARTH_CAST_TIME)
+			get_node("Timer").start()
+			get_node("AnimationPlayer").play("cast_earth")
 	
 	# ugly but Meh!
 	for i in range(0, time_to_move):
 		if get_pos().distance_to(actual_pos) <= 0.1:
 			moving = false
 		
-		if !moving:
-			var direction = handle_input()
+		if !moving and current_mode != MODE_EARTH and current_mode != MODE_CAST:
+			var direction = handle_direction_input()
 			
 			if direction != MOVE_NONE:
 				moving = true
@@ -57,7 +84,12 @@ func _fixed_process(delta):
 			motion = motion * delta * speed 
 			move(motion)
 	
-func handle_input():
+func timer_end():
+	get_node("Timer").stop()
+	get_node("AnimationPlayer").play("idle")
+	current_mode = MODE_NORMAL
+	
+func handle_direction_input():
 	if ( Input.is_action_pressed("move_up") ):
 		return MOVE_UP
 	if ( Input.is_action_pressed("move_down") ):
@@ -66,10 +98,27 @@ func handle_input():
 		return MOVE_LEFT
 	if ( Input.is_action_pressed("move_right") ):
 		return MOVE_RIGHT
-	return MOVE_NONE	
+	return MOVE_NONE
+	
+func handle_cast_input():
+	if ( Input.is_action_pressed("cast_fire") ):
+		return MODE_FIRE
+	if ( Input.is_action_pressed("cast_water") ):
+		return MODE_WATER
+	if ( Input.is_action_pressed("cast_earth") ):
+		return MODE_EARTH
+	if ( Input.is_action_pressed("cast_air") ):
+		return MODE_AIR
+	return MODE_CAST_NONE
 
 func is_tile_solid(tile):	
 	return get_parent().get_node("map").is_tile_solid(tile);
 	
 func get_tile_type(pos):
 	return get_parent().get_node("map").get_tile_type(pos);
+	
+func get_mode():
+	return self.current_mode;
+	
+func get_face():
+	return self.current_mode;
