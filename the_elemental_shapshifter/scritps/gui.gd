7eb13a7acf5bac13c8780c6cb2486d5ca219ef26
Paulb23 <p_batty@hotmail.co.uk>
2016-04-16 14:22:49 +0100

    gui start

---
+++ the_elemental_shapshifter/scritps/gui.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	var player = get_parent().get_node("Player")
+	
+	if (player):
+		get_node("fire_cooldown_text").set_text(str(player.get_node("fire_cooldown").get_time_left()))
+		get_node("water_cooldown_text").set_text(str(player.get_node("water_cooldown").get_time_left()))
+		get_node("earth_cooldown_text").set_text(str(player.get_node("earth_cooldown").get_time_left()))
+		get_node("air_cooldown_text").set_text(str(player.get_node("air_cooldown").get_time_left()))	
