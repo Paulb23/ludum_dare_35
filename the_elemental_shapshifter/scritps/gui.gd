75337b7cd06e7a8d742a4b42f3b54f96ee6a53a2
Paulb23 <p_batty@hotmail.co.uk>
2016-04-17 15:25:51 +0100

    Spell icons

--- the_elemental_shapshifter/scritps/gui.gd
+++ the_elemental_shapshifter/scritps/gui.gd
@@ -19,25 +19,41 @@ func _fixed_process(delta):
 	
 	if (player):
 		get_node("health_text").set_text(str(player.get_health()))
-		get_node("fire_cooldown_text").set_text(str(player.get_node("fire_cooldown").get_time_left()))
-		get_node("water_cooldown_text").set_text(str(player.get_node("water_cooldown").get_time_left()))
-		get_node("earth_cooldown_text").set_text(str(player.get_node("earth_cooldown").get_time_left()))
-		get_node("air_cooldown_text").set_text(str(player.get_node("air_cooldown").get_time_left()))	
+		
+		if player.get_node("fire_cooldown").get_time_left() == 0:
+			get_node("fire_sprite").set_frame(0)
+		else:
+			get_node("fire_sprite").set_frame(1)
+			
+		if player.get_node("water_cooldown").get_time_left() == 0:
+			get_node("water_sprite").set_frame(0)
+		else:
+			get_node("water_sprite").set_frame(1)
+		
+		if player.get_node("earth_cooldown").get_time_left() == 0:
+			get_node("earth_sprite").set_frame(0)
+		else:
+			get_node("earth_sprite").set_frame(1)
+			
+		if player.get_node("air_cooldown").get_time_left() == 0:
+			get_node("air_sprite").set_frame(0)
+		else:
+			get_node("air_sprite").set_frame(1)
 		
 func hide_gui():
 	get_node("clock").hide()
 	get_node("health_text").hide()
-	get_node("fire_cooldown_text").hide()
-	get_node("water_cooldown_text").hide()
-	get_node("earth_cooldown_text").hide()
-	get_node("air_cooldown_text").hide()
+	get_node("fire_sprite").hide()
+	get_node("water_sprite").hide()
+	get_node("earth_sprite").hide()
+	get_node("air_sprite").hide()
 	current_mode = MODE_HIDDEN
 	
 func show_gui():
 	get_node("clock").show()
 	get_node("health_text").show()
-	get_node("fire_cooldown_text").show()
-	get_node("water_cooldown_text").show()
-	get_node("earth_cooldown_text").show()
-	get_node("air_cooldown_text").show()
+	get_node("fire_sprite").show()
+	get_node("water_sprite").show()
+	get_node("earth_sprite").show()
+	get_node("air_sprite").show()
 	current_mode = MODE_ACTIVE
